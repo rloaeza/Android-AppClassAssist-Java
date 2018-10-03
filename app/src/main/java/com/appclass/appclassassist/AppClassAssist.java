10cb49c386c334c47850ea7fa4e6d240dc383eeb
roberto <roberto.loaeza@gmail.com>
2018-10-02 22:26:40 -0500

    Registrando

--- app/src/main/java/com/appclass/appclassassist/AppClassAssist.java
+++ app/src/main/java/com/appclass/appclassassist/AppClassAssist.java
@@ -4,28 +4,54 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class AppClassAssist extends AppCompatActivity {
     ImageView ivAsistir;
     private int tiempoVisible=60;
     private static int btRequestCode = 10;
+
+    private Button bRegistrar;
+    private EditText etCorreo;
+    private EditText etCodigo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_class_assist);
 
-        ivAsistir = findViewById(R.id.ivAsistir);
+        BluetoothAdapter BTAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(BTAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.noCompatible))
+                    .setMessage(getString(R.string.noBluetooth))
+                    .setPositiveButton(getString(R.string.salir), (dialog, which) -> System.exit(0))
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+        else {
 
 
-        ivAsistir.setOnClickListener(e->{
-            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, tiempoVisible);
-            startActivityForResult(discoverableIntent, btRequestCode);
+            ivAsistir = findViewById(R.id.ivAsistir);
+            bRegistrar = findViewById(R.id.bRegistrar);
 
-        });
+            ivAsistir.setOnClickListener(e -> {
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, tiempoVisible);
+                startActivityForResult(discoverableIntent, btRequestCode);
+
+            });
+
+            bRegistrar.setOnClickListener(e -> {
+                Intent intent = new Intent(this, AppClassRegistrar.class);
+                startActivity(intent);
+            });
+        }
 
     }
 
---
+++ app/src/main/java/com/appclass/appclassassist/AppClassReferencias.java
@@ -0,0 +1,20 @@
+package com.appclass.appclassassist;
+public class AppClassReferencias {
+
+    public static String TAG_Debug = "AppClass_Debug";
+    public static String AppClass = "AppClass";
+    public static String Personas = "instructores";
+    public static String Clases = "clases";
+    public static String Alumnos = "alumnos";
+    public static String Asistencias = "asistencias";
+
+    public static int tamCodigo = 10;
+
+
+    public static String  claseCodigo = "clase Codigo";
+    public static String  claseNombre = "clase Nombre";
+
+    public static  String bdNombreClase = "nombreClase";
+    public static  String bdCantidadAlumnos = "cantidadAlumnos";
+    public static  String bdAsistio = "asistio";
+}
